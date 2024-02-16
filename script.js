879ccc3d3eb0bace68c24f245814b2b76bd8dcd6
Itobuz <itobuz@Itobuzs-MacBook-Pro.local>
2024-02-16 17:25:01 +0530

    Counter updated and complete now

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const count = document.getElementsByClassName("number")[0];
+const count = document.querySelector(".number");
+const allButtons = document.querySelector(".buttons");
 
-function color(num){
-    if(num > 0) count.style.color = "green";
-    else if(num < 0) count.style.color = "red";
+function color(num) {
+    if (num > 0) count.style.color = "green";
+    else if (num < 0) count.style.color = "red";
     else count.style.color = "black"
 }
 
-function increment() {
-    let num = parseInt(count.innerHTML);
-    num++;
-    color(num);
-    count.innerText = num;
-}
-
-function resetToZero() {
-    num = 0;
-    color(num);
-    count.innerText = num;
-}
-
-function decrement() {
-    let num = parseInt(count.innerHTML);
-    num--;
-    color(num);
-    count.innerText = num;
-}
+allButtons.addEventListener('click', (e) => {
+    const buttonId = e.target.dataset.buttonid;
+    let num = parseInt(count.innerText);
+    if (buttonId === "1") {
+        num++;
+        color(num);
+        count.innerText = num;
+    }
+    else if (buttonId === "2") {
+        num = 0;
+        color(num);
+        count.innerText = num;
+    }
+    else if (buttonId === "3") {
+        num--;
+        color(num);
+        count.innerText = num;
+    }
+});
